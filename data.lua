0bda2b9d7bd059aef5ab564c02ae6f5164184876
Jack Honour <JaxkDev@gmail.com>
2021-07-03 21:38:17 +0100

    Initial structure + test settings/locale.

---
+++ data.lua
@@ -0,0 +1,9 @@
+---------------------------------------------------------------------------
+-- Data stage #1
+---------------------------------------------------------------------------
+-- Internal Info
+zoctorio = {}
+zoctorio.internal_name = "zoctorio"
+zoctorio.name = "Zoctorio"
+zoctorio.version = mods[zoctorio.internal_name]
+zoctorio.stage = "data"
---
+++ settings.lua
@@ -0,0 +1,13 @@
+data:extend(
+{
+  {
+    type = "double-setting",
+    name = "soil-richness",
+    setting_type = "startup",
+    default_value =  1.0,
+    minimum_value = 0.2,
+    maximum_value = 2.0,
+    order = "a-a",
+  }
+}
+)
